05c399e8a306d60f77c7fa7ade800773f9ba0606
Luke Brogee <lukebrogee@gmail.com>
2023-11-30 16:45:26 -0500

    Add project files.

---
+++ InItToWinIt/InItToWinIt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* This program will display a songs name, artist,
+and time length for Karaoke*/
+
+int main() {
+	std::cout << "Karaoke\n";
+	std::cout << "Rap God\n";
+	std::cout << "As performed by: Eminem\n";
+	std::cout << "Time: 6:09" << std::endl;
+
+	return 0;
+}
